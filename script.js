9b2509935073e6414525d798a9e4589ff26cf862
rednax28 <kiritosalas1234@gmail.com>
2025-12-01 17:21:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let input = document.querySelector("input");
+let buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        let value = button.innerText;
+
+        if (value === "AC") {
+            expression = "";
+            input.value = "0";
+        } 
+        else if (value === "DEL") {
+            expression = expression.slice(0, -1);
+            input.value = expression || "0";
+        }
+        else if (value === "=") {
+            try {
+                expression = eval(expression).toString();
+                input.value = expression;
+            } catch (error) {
+                input.value = "Error";
+                expression = "";
+            }
+        } 
+        else {
+            if (input.value === "0" && value !== ".") {
+                expression = value;
+            } else {
+                expression += value;
+            }
+            input.value = expression;
+        }
+    });
+});
